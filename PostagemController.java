d58cd277b0559536ca31652d68dff1ae02a192b5
kemellynasc <72822516+kemellynasc@users.noreply.github.com>
2022-04-29 14:13:27 -0300

    Add files via upload

---
+++ PostagemController.java
@@ -0,0 +1,27 @@
+package com.BlogPessoal.BlogPessoal.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.BlogPessoal.BlogPessoal.Repository.PostagemRepository;
+import com.BlogPessoal.BlogPessoal.model.Postagem;
+
+@RestController
+ @RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
